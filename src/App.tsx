c1ad29718f2824b3e01f3aa0802e96665ba78c48
Leonso Medina <leomedina.me@gmail.com>
2026-02-05 13:52:26 -0600

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen min-w-[320px] bg-slate-900 text-white">
+      <p className="mt-6 text-sm text-slate-400">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default App;
